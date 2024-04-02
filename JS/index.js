2945b9a7a24fc6cc8a3c5f4da94ae8f7323ae90e
Andrey <108063187+Minaht007@users.noreply.github.com>
2024-04-02 12:17:51 +0300

    change in part one

--- JS/index.js
+++ JS/index.js
@@ -2,11 +2,31 @@
 const input = document.querySelector(".input")
 const btm = document.querySelector(".btm")
 const textarea = document.querySelector(".textarea")
+const p = document.querySelector(".p")
 
 function text(e) {    
     e.preventDefault();
     const textInput = input.value
-    textarea.value = textInput    
+    textarea.value = textInput 
+    p.textContent = textInput   
 }
 
 btm.addEventListener("click", text)
+
+// part two
+
+
+
+let textTransfer = false;
+
+let startX
+let endX
+let startY
+let endY
+
+
+p.addEventListener('mousedown', function(e) {
+    textTransfer = true
+    startX = e.value
+    startY = e.value
+})
